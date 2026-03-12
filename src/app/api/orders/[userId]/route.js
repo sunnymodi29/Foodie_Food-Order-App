b50de20a27b726e7706de4cdfc64705dedfa022a
sunnymodi29 <modisunny25@gmail.com>
2026-03-12 14:14:03 +0530

    feat: Add API route to fetch user orders by ID, including admin access and calculated totals.

--- src/app/api/orders/[userId]/route.js
+++ src/app/api/orders/[userId]/route.js
@@ -1,6 +1,7 @@
 import pool from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export async function GET(request, { params }) {
